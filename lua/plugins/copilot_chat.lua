738cd46f08040b575c0f598059b88efcb133f0c3
Roma <totsamyroma@gmail.com>
2025-07-31 14:39:33 +0300

    Open copilot in the bottom

--- lua/plugins/copilot_chat.lua
+++ lua/plugins/copilot_chat.lua
@@ -1,11 +1,12 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    tag = "v3.12.2",
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    build = "make tiktoken",                          -- Only on MacOS or Linux
     opts = {
       mappings = {
         complete = {
@@ -15,8 +16,11 @@ return {
           insert = "<C-r>",
           normal = "<C-r>",
         },
-      }
+      },
       -- See Configuration section for options
+      window = {
+        layout = 'horizontal', -- 'horizontal' or 'vertical'
+      }
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
