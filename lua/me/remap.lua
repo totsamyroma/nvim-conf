ddde7006363a64d01a40de152fa1c1be24ece988
Roma <totsamyroma@gmail.com>
2025-07-31 14:38:16 +0300

    Put window right/below on split

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -7,3 +7,7 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- move lines up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- open split window and put it right/below
+vim.opt.splitright = true
+vim.opt.splitbelow = true
