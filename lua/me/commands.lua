1b6bc9c38b6167df7393ba44ba2d57bf41e7f5b1
Roma <totsamyroma@gmail.com>
2025-09-05 00:55:18 +0300

    Autoformat json

--- lua/me/commands.lua
+++ lua/me/commands.lua
@@ -5,3 +5,10 @@ end, {})
 vim.api.nvim_create_user_command('Spec', function()
     vim.cmd('term zsh -lic spec %')
 end, {})
+
+vim.api.nvim_create_autocmd("FileType",  {
+    pattern = { "json" },
+    callback = function()
+      vim.api.nvim_set_option_value("formatprg", "jq", { scope = 'local' })
+    end,
+})
