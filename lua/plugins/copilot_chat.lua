7b6b3180b884bffed9c4b351a37520d3127b44f8
Roma <totsamyroma@gmail.com>
2026-01-30 20:47:45 +0300

    Bumped CopilotChat version

--- lua/plugins/copilot_chat.lua
+++ lua/plugins/copilot_chat.lua
@@ -1,7 +1,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    tag = "v3.12.2",
+    tag = "v4.7.4",
     dependencies = {
       { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
